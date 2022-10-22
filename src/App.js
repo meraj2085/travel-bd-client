b3a847147f9519e6d3f6e35a6738f4d75dcc58eb
meraj2085 <samirmeraj60@gmail.com>
2022-10-22 20:47:21 +0600

    adding daisy ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>Hlw world</h1>
+      <h1>hlw world</h1>
     </div>
   );
 }
